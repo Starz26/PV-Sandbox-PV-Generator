47074de5ade7b3ee5f5c158dccf1bfd8ce4a7f84
divyesh <divyesh_doshi@silvertouch.com>
2012-11-05 18:20:12 +0530

    test

--- src/main/java/com/example/ChartGeneration.java
+++ src/main/java/com/example/ChartGeneration.java
@@ -435,7 +435,7 @@ public class ChartGeneration {
 			
 			// url call for SF public web service
 			//"https://abbott-integration.fbd.cs11.force.com/PV_GenerateChartImageForPvId"
-			URL url = new URL("https://abbott-integration.fbd.cs11.force.com/PV_GenerateChartImageForPvId");
+			URL url = new URL(URLEncoder.encode(sfPublicUrl, "UTF-8"));
  
 			// URLConnection con = url.openConnection();
 			// for trusted connection
